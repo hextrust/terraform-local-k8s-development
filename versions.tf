55663a5f2f15c30cafea57e8820b1609ed6817e8
Aaron Baideme <aaron.baideme@hextrust.com>
2023-04-17 23:17:54 +0800

    fix: updating required versions

--- versions.tf
+++ versions.tf
@@ -10,6 +10,10 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.11"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14"
+    }
     null = {
       source  = "hashicorp/null"
       version = "~> 3.2"
